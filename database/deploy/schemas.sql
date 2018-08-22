fd3de4db15f00f98b0f11378dc1cca8da7b7111a
Luis Amigo <lamigo@praderas.org>
2018-08-22 13:41:57 +0200

    added comments to schemas

--- database/deploy/schemas.sql
+++ database/deploy/schemas.sql
@@ -4,11 +4,19 @@ BEGIN;
 
 -- public schema in which we publish data
 CREATE SCHEMA api;
+COMMENT ON SCHEMA api IS
+'Public api with read methods to view data';
 
 -- schema used to import source to
 CREATE SCHEMA source;
 
+COMMENT ON SCHEMA source IS
+'Public API to submit data, only PUT methods are supported';
+
 -- schema used for private data
 CREATE SCHEMA private;
+
+COMMENT ON SCHEMA private IS
+  'private schema for data transformation';
 
 COMMIT;
